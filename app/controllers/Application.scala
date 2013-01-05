a572bc55891c48aa1015a538acd213f7414a6525
Cedric Gatay <cedric@gatay.fr>
2013-01-05 11:51:05 +0100

    Logging error

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,19 +3,13 @@ package controllers
 import play.api.mvc._
 import scala._
 import scala.Some
+import services.Answerer
 
 object Application extends Controller {
 
   def index = Action {
     implicit request =>
-      answerQuery(request.queryString.get("q"))
+      Answerer.answerQuery(request.queryString.get("q"))
   }
 
-
-  def answerQuery(query: Option[Seq[String]]) : Result = {
-    query match {
-      case Some(Seq("Quelle est ton adresse email")) => Ok("cedric@gatay.fr")
-      case _ => NotFound
-    }
-  }
 }
--- app/services/Answerer.scala
+++ app/services/Answerer.scala
@@ -1,10 +1,24 @@
 package services
 
+import play.api.mvc._
+import play.api.mvc.Results._
+import scala._
+import scala.Some
+import play.api.Logger
+
 /**
  * User: cgatay
  * Date: 05/01/13
  * Time: 11:23
  */
 object Answerer {
-
+  def answerQuery(query: Option[Seq[String]]) : Result = {
+    query match {
+      case Some(Seq("Quelle est ton adresse email")) => Ok("cedric@gatay.fr")
+      case _ => {
+        Logger.error("No match : " + query)
+        NotFound
+      }
+    }
+  }
 }
