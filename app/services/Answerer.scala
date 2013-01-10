557f91776ed9dbd0b6e8f5488e5ae15780a712e3
Cedric Gatay <cedric@gatay.fr>
2013-01-10 11:16:56 +0100

    Tiny refactor of the pattern matching, removed grouping

--- app/services/Answerer.scala
+++ app/services/Answerer.scala
@@ -17,11 +17,11 @@ object Answerer {
   def answerQuery(query: Option[Seq[String]]) : Result = {
     val queryContent : Seq[String]= query.getOrElse(Seq())
     val firstQueryContent = queryContent.headOption.getOrElse("")
-    val ouiNonPattern = ".*(OUI/NON\\))$".r
+    val OuiNonPattern = ".*OUI/NON\\)$".r
     firstQueryContent match {
       case "Quelle est ton adresse email"
         => Ok("cedric@gatay.fr")
-      case ouiNonPattern(_)
+      case OuiNonPattern()
         => Ok("OUI")
       case _ => {
         Logger.error("No match : " + query)
