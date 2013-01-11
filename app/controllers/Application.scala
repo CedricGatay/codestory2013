de146dfb6342a9e287a1eb4eef536c94dd0799d0
Cedric Gatay <cedric@gatay.fr>
2013-01-11 16:57:58 +0100

    Refactored to switch on http verb

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,20 +4,21 @@ import play.api.mvc._
 import services.Answerer
 import play.api.data._
 import play.api.data.Forms._
+import play.api.Logger
 
 object Application extends Controller {
-  val postForm = Form("q" -> text)
 
   def index = Action {
     implicit request =>
-      postForm.bindFromRequest.fold(
-        error => {
-          Answerer.answerQuery(request.queryString.get("q"))
-        },
-        success => {
-          Answerer.answerQuery(Some(Seq(success)))
+      request.method match {
+        case "GET" => Answerer.answerQuery(request.queryString.get("q"))
+        case "POST" => {
+          val encoded = request.body.asFormUrlEncoded
+          Logger.info("Posted the following data : %s".format(encoded))
+          Answerer.answerQuery(encoded.getOrElse(Map()).get("q"))
+          Status(201)
         }
-      )
+      }
   }
 
 }
